c0177febfc9f0491c840370352e205f1fade259a
rtran1029 <103476583+rtran1029@users.noreply.github.com>
2022-07-26 00:10:22 -0400

    creating employee lib

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const inquirer = require('inquirer');
+const inquirer = import('inquirer');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
